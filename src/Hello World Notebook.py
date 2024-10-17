c6f7b252d3f932dfa78dd76865da400e9466a055
yangwang166 <yangwang166>
2024-10-17 13:18:19 +0000

    feature 2 commit
    feature 2 commit

--- src/Hello World Notebook.py
+++ src/Hello World Notebook.py
@@ -4,4 +4,4 @@ dbutils.widgets.dropdown("Who Am I", "Will Wang", ["Will Wang", "Jeremy Wang", "
 # COMMAND ----------
 
 name = dbutils.widgets.get("Who Am I")
-print(f"My name is {name}, I am from databricks")
+print(f"My name is {name}, I am from databricks， I am a machine learning engineer")
